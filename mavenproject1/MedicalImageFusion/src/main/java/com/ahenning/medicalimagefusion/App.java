85f3a6884fa6cd412bfa7f090c7f9e272ad58daf
Arthur Henning <h_arthur_12@yahoo.com>
2014-01-04 18:39:37 +0200

    Added dicom image and info saving functionality. Modified .gitignore.

--- mavenproject1/MedicalImageFusion/src/main/java/com/ahenning/medicalimagefusion/App.java
+++ mavenproject1/MedicalImageFusion/src/main/java/com/ahenning/medicalimagefusion/App.java
@@ -1,6 +1,13 @@
 package com.ahenning.medicalimagefusion;
 
 import ij.plugin.DICOM;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  * Hello world!
@@ -8,11 +15,32 @@ import ij.plugin.DICOM;
  */
 public class App 
 {
+
+    private static final String processingFolder
+            = "images/";
+    private static final String sourceFolder
+            = "E:\\Projects\\MedicalImageFusion\\Dicom files\\";
     public static void main( String[] args )
     {
-        System.out.println("Opening DICOM file!");
+        //opening DICOM file
         DICOM dcm = new DICOM();
-        dcm.open("E:\\Projects\\MedicalImageFusion\\Dicom files\\dicom.dcm");
-        System.out.println("Image height " + dcm.getHeight() + " width: " + dcm.getWidth() + "\nInfo\n" + dcm.getProperties());
+        dcm.open(sourceFolder + "dicom.dcm");
+
+        //extract image data from DICOM file
+        BufferedImage bf = dcm.getBufferedImage();
+        PrintWriter writer = null;
+        String currentImageFolder
+                = String.valueOf(System.currentTimeMillis()) + "/";
+        File outputfile = new File(processingFolder + currentImageFolder + "image.jpg");
+        outputfile.mkdirs();
+        try {
+            ImageIO.write(bf, "jpg", outputfile);
+            writer = new PrintWriter(processingFolder + currentImageFolder + "imageInfo.txt", "UTF-8");
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        writer.println(dcm.getProperties());
+        writer.close();
     }
 }
